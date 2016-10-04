1ff484816d3224719424d1e3312bdcdb3acc1a6c
Salvo <salvo.cannamela@gmail.com>
2016-10-04 22:10:47 +0100

    First base class Basket

---
+++ app/src/main/java/com/firebox/androidapp/util/Basket.java
@@ -0,0 +1,13 @@
+package com.firebox.androidapp.util;
+
+/**
+ * Created by salvo on 03/10/16.
+ */
+public class Basket {
+
+    static Boolean addToBasket(int skuId)
+    {
+
+        return true;
+    }
+}
